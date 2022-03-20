e12025ea83ff9fabc46926d6931934b67cea19a1
sunshine09101 <sunshine09101@gmail.com>
2022-03-20 22:19:31 +0100

    space removed

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
 			<img className="width-100 website-hero-back" src="./assets/images/new_banner.png"/>
 			<img className="width-100 mobile-hero-back" src="./assets/images/new_banner_mobile.png"/>
 		</section>
-
 		<div className="main-container">
 			<section className="about-section">
 				<div className="col-12 text-center d-flex justify-content-center wow fadeInUp">
